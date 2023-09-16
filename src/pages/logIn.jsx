560953b7513cedc21c2df4b6b8451f65ef7cbc85
Bhiekeys <tinuolajisodimu@gmail.com>
2023-09-16 09:02:48 +0100

    signin page adjusted

--- src/pages/logIn.jsx
+++ src/pages/logIn.jsx
@@ -7,10 +7,10 @@ const Login = () => {
         <form className="pt-40 flex justify-center  pb-5">
             <div className="">
                 <div className="text-center">
-                    <h2 className="text-[#000] text-[35px] font-bold">
+                    <h2 className="text-[#000] mb-2 text-xl sm:text-[35px] font-bold">
                         Login to your account
                     </h2>
-                    <p className="text-[#000000] font-[500] text-[16px]">
+                    <p className="text-[#000000] text-sm font-[500] sm:text-[16px]">
                         Please enter your details to proceed further
                     </p>
                 </div>
@@ -28,11 +28,11 @@ const Login = () => {
                     <Button text=" Login" />
                 </div>
                 <div>
-                    <p className="text-todo-blue-2 text-center pt-5 font-[500] text-[16px]">
+                    <p className="text-todo-blue-2 text-center pt-5 font-[500] text-sm sm:text-[16px]">
                         Don't have an account?
                         <Link
                             to="/signup"
-                            className="text-bg-todo-blue-2 pl-1 font-[500] text-[16px] underline underline-offset-2"
+                            className="text-bg-todo-blue-2 pl-1 font-[500]  underline underline-offset-2"
                         >
                             Sign up
                         </Link>
